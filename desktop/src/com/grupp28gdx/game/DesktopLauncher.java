fe84d55402112f56329ee724dea8e369d6a85d50
Tussies <86693581+Tussies@users.noreply.github.com>
2022-09-04 19:30:02 +0200

    setup the project

---
+++ desktop/src/com/grupp28gdx/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.grupp28gdx.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.grupp28gdx.game.Grupp28GDX;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Grupp28_GDX");
+		new Lwjgl3Application(new Grupp28GDX(), config);
+	}
+}
