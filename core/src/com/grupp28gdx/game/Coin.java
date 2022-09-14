4abbf47754835934e6b277c706cd62daa4d5d540
Annheij <ann.heijkenskjold@gmail.com>
2022-09-14 11:37:46 +0200

    Added CoinHandler that can generate coins, and CircleShape for coins

--- core/src/com/grupp28gdx/game/Coin.java
+++ core/src/com/grupp28gdx/game/Coin.java
@@ -1,5 +1,17 @@
 package com.grupp28gdx.game;
 
-public class Coin extends Actor{
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 
+public class Coin extends Actor{
+    private int posX;
+    private int posY;
+    CircleShape coin;
+
+    public Coin(){
+        posX = this.getX_position();
+        posY = this.getY_position();
+        coin = new CircleShape();
+        coin.setRadius(10);
+    }
 }
