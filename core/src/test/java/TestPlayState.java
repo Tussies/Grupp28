773a11d3f0fa2617ce2b8498890c849ba81229fe
elzorroartico <gilabert.alexander@gmail.com>
2022-09-14 23:36:30 +0200

    Finished all tests for Actor Class

--- core/src/test/java/TestPlayState.java
+++ core/src/test/java/TestPlayState.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class TestPlayState {
+import com.badlogic.gdx.physics.box2d.Body;
+import com.grupp28gdx.game.states.GameStateManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+import com.grupp28gdx.game.states.PlayState;
+
+public class TestPlayState {
+
+
+    /* @Test
+    public void testCreatePlayer(){
+        PlayState testBody = new PlayState(gsm);
+        assertTrue(1==1);
+    }*/
+
 }
+
+
