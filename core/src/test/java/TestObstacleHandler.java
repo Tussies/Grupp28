5bca81321561bb8158d738baafe44220ccd8a689
elzorroartico <gilabert.alexander@gmail.com>
2022-09-15 04:17:01 +0200

    Started on the tests for ObstacleHandler

---
+++ core/src/test/java/TestObstacleHandler.java
@@ -0,0 +1,10 @@
+import com.grupp28gdx.game.ObstacleHandler;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Array;
+
+public class TestObstacleHandler {
+    @Test
+    ObstacleHandler testObstacleArray = new ObstacleHandler();
+    assertArrayEquals(new Array<>, testObstacleArray.);
+}
