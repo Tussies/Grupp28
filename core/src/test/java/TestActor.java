2267b6a1093afea05ef14cb0d90d26bf344770dd
Tussies <86693581+Tussies@users.noreply.github.com>
2022-09-23 15:22:02 +0200

    trying to fetch changes

--- core/src/test/java/TestActor.java
+++ core/src/test/java/TestActor.java
@@ -3,9 +3,6 @@ import static org.junit.Assert.assertTrue;
 
 public class TestActor {
     /*Actor testActor = new Actor();
-
-
-
     @Test
     public void testGetXPositions(){
         assertTrue(testActor.getX_position() == 0);
@@ -17,11 +14,9 @@ public class TestActor {
     @Test
     public void testGetXDirection(){
         assertTrue(testActor.getX_direction() == 0);
-
     }
     @Test
     public void testGetYDirection(){
         assertTrue(testActor.getY_direction() == 0);
-    }
+    }*/
 }
-
