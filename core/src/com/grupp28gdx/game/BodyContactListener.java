d3a4569461b6e3dfb938d250899f9632ef6f89fe
Tussies <86693581+Tussies@users.noreply.github.com>
2022-09-25 20:26:14 +0200

    added contact listener class

---
+++ core/src/com/grupp28gdx/game/BodyContactListener.java
@@ -0,0 +1,34 @@
+package com.grupp28gdx.game;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.grupp28gdx.game.states.PlayState;
+
+public class BodyContactListener implements ContactListener {
+
+    private PlayState parent;
+
+    public BodyContactListener(PlayState parent){
+        this.parent = parent;
+    }
+
+    @Override
+    public void beginContact(Contact contact) {
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+    }
+
+}
