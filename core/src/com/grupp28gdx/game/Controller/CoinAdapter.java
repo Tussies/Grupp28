4f1dc5e3f581c204e9af7ed07f3a33d1392b3512
Annheij <ann.heijkenskjold@gmail.com>
2022-10-06 00:39:37 +0200

    added empty voids for now

--- core/src/com/grupp28gdx/game/Controller/CoinAdapter.java
+++ core/src/com/grupp28gdx/game/Controller/CoinAdapter.java
@@ -1,4 +1,20 @@
 package com.grupp28gdx.game.Controller;
+import java.awt.*;
+import java.awt.event.*;
+
+import com.grupp28gdx.game.Model.Coin;
 
 public class CoinAdapter {
+    Coin c;
+    CoinAdapter(){
+    }
+
+public void onTouch(){
+}
+
+public void setPoints(){
+
+}
+
+
 }
