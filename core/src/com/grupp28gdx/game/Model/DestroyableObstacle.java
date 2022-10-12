30259bbc04647a32b4c9a4450e4b1e90dfbe9b22
Annheij <ann.heijkenskjold@gmail.com>
2022-10-12 15:45:19 +0200

    added methods createShape and destroyShape, destroyShape to work on

--- core/src/com/grupp28gdx/game/Model/DestroyableObstacle.java
+++ core/src/com/grupp28gdx/game/Model/DestroyableObstacle.java
@@ -7,19 +7,52 @@ public class DestroyableObstacle implements Obstacle {
     private ObstacleShape shape;
     private ObstacleColor color; //this color makes the destroyable obstacle distinguishable from the permanent obstacles
     private Body body;
+    private float[] vectorListX;
+    private float[] vectorListY;
+    private boolean destroyObstacle;
+
 
     public DestroyableObstacle(float spawnPosX, float spawnPosY) {
         shape = ObstacleShape.SQUARE;
         color = ObstacleColor.BLUE;
-        this.body = new Body(0,0);
+        this.body = new Body(spawnPosX,spawnPosY);
         this.body.setXPosition(0);
         this.body.setYPosition(0);
+        createShape();
+        if (destroyObstacle){
+            destroyShape();
+        };
     }
+
+    private void createShape() {
+        vectorListX = new float[]{
+                body.getXPosition(),
+                body.getXPosition(),
+                body.getXPosition() + 1,
+                body.getXPosition() + 1
+        };
+        vectorListY = new float[]{
+                body.getXPosition(),
+                body.getXPosition()+2,
+                body.getXPosition()+2,
+                body.getXPosition()
+        };
+    }
+
+    private void destroyShape() {
+
+    }
+
 
     @Override
     public Obstacle createObstacle(float spawnX, float spawnY) {
         return new DestroyableObstacle(spawnX,spawnY);
     }
+
+    public Body getPosition(){
+        return body;
+    }
+
 
     public ObstacleShape getShape() {
         return shape;
