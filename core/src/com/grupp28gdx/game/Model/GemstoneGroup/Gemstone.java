87807e84e3c81db80dc057d21ef1cf2d6f0a3db9
Nanooss <Isak.wideskott@gmail.com>
2022-10-14 14:00:18 +0200

    Added Collision Detection Class

--- core/src/com/grupp28gdx/game/Model/GemstoneGroup/Gemstone.java
+++ core/src/com/grupp28gdx/game/Model/GemstoneGroup/Gemstone.java
@@ -1,9 +1,11 @@
 package com.grupp28gdx.game.Model.GemstoneGroup;
 
+import com.grupp28gdx.game.Model.Body;
 import com.grupp28gdx.game.Model.Player;
 
 public interface Gemstone {
     Gemstone createGemstone();
     void updateGemCounter();
     float getSideLength();
+    Body getBody();
 }
