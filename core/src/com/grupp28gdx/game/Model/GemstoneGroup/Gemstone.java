fe1f52e57d044ba1b4dd8a1ec09d377d4d092b13
Annheij <ann.heijkenskjold@gmail.com>
2022-10-15 13:48:02 +0200

    JavaDoced my classes

--- core/src/com/grupp28gdx/game/Model/GemstoneGroup/Gemstone.java
+++ core/src/com/grupp28gdx/game/Model/GemstoneGroup/Gemstone.java
@@ -3,6 +3,10 @@ package com.grupp28gdx.game.Model.GemstoneGroup;
 import com.grupp28gdx.game.Model.Body;
 import com.grupp28gdx.game.Model.Player;
 
+/**
+ * This is the common interface for the different type of objects. There are three types of gem objects in the game
+ * in total for the player to collect. If the player makes contact with a gem the point counter goes up.
+ */
 public interface Gemstone {
     Gemstone createGemstone();
     void updateGemCounter();
