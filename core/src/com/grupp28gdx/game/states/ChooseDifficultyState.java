f56aa76b752436397898867f25469a65290d32c4
Annheij <ann.heijkenskjold@gmail.com>
2022-10-17 17:16:45 +0200

    Added state class

---
+++ core/src/com/grupp28gdx/game/states/ChooseDifficultyState.java
@@ -0,0 +1,38 @@
+package com.grupp28gdx.game.states;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class ChooseDifficultyState extends State{
+    private Texture background;
+    private Texture easyButton;
+    private Texture easyButtonPressed;
+    private Texture normalButton;
+    private Texture normalButtonPressed;
+    private Texture hardButton;
+    private Texture hardButtonPressed;
+
+    protected ChooseDifficultyState(GameStateManager gsm){
+        super(gsm);
+
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
