ce85e0528aa0987775e10d56109908179bf2040d
elzorroartico <gilabert.alexander@gmail.com>
2022-10-24 11:01:43 +0200

    Testing obstacle and collision begin

--- core/src/test/java/ObstacleGroupTest/SpikeObstacleTest.java
+++ core/src/test/java/ObstacleGroupTest/SpikeObstacleTest.java
@@ -1,42 +1,41 @@
 package ObstacleGroupTest;
 
 import com.grupp28gdx.game.Model.Body;
-import com.grupp28gdx.game.Model.DestroyableObstacle;
 import com.grupp28gdx.game.Model.Obstacle;
+import com.grupp28gdx.game.Model.SpikeObstacle;
 import org.junit.Test;
+
 import static org.junit.Assert.assertTrue;
 
-public class DestroyableObstacleTest {
+public class SpikeObstacleTest {
 
-    DestroyableObstacle destroyObst = new DestroyableObstacle(2, 2, 1);
+    SpikeObstacle spikeObs = new SpikeObstacle(1,1,1);
 
     @Test
     public void createObstacle() {
-        Obstacle temp = destroyObst.createObstacle(1, 1, 1);
+        Obstacle temp = spikeObs.createObstacle(1, 1, 1);
         assertTrue(temp instanceof Obstacle);
     }
 
     @Test
     public void getBody() {
-        assertTrue(destroyObst.getBody() instanceof Body);
+        assertTrue(spikeObs.getBody() instanceof Body);
     }
 
     @Test
     public void getId() {
-        assertTrue(destroyObst.getId() == 1);
+        assertTrue(spikeObs.getId() == 1);
     }
 
     @Test
     public void getWidth() {
-        assertTrue(destroyObst.getWidth() == 1f);
+        assertTrue(spikeObs.getWidth() == 1f);
     }
 
     @Test
     public void getHeight() {
-        assertTrue(destroyObst.getHeight() == 1.9f);
+        assertTrue(spikeObs.getHeight() == 1.9f);
     }
-
-
 
 
 }
