efb9a75915e28a823512c40c68082a57b6305b20
elzorroartico <gilabert.alexander@gmail.com>
2022-10-24 11:13:30 +0200

    Player Test an  d renderview

---
+++ core/src/com/grupp28gdx/game/render/RenderView.java
@@ -0,0 +1,74 @@
+package com.grupp28gdx.game.render;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class RenderView {
+    private SpriteBatch sb;
+    public RenderView(){
+        sb = new SpriteBatch();
+    }
+
+    private Music music;
+
+    public void render(Texture texture, int x, int y){
+        sb.begin();
+        sb.draw(texture,x,y);
+        sb.end();
+    }
+
+    public void render(Texture texture,float x,float y,float screenWith,float screenHeight){
+        sb.begin();
+        sb.draw(texture,x,y,screenWith,screenHeight);
+        sb.end();
+    }
+
+    public void debugRender(Box2DDebugRenderer b2ddr, World world, OrthographicCamera cam, float pixelsPerMeter){
+        b2ddr.render(world, cam.combined.scl(pixelsPerMeter));
+
+    }
+
+    public void updateCamera(Camera cam, Vector3 position){
+        cam.position.set(position);
+        cam.update();
+    }
+
+    public void setProjectionMatrix(Matrix4 combined) {
+        sb.setProjectionMatrix(combined);
+    }
+
+    public void updateScore(int score){
+        sb.begin();
+        sb.end();
+    }
+
+    public void render(Hud hud) {
+        sb.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    public void renderMusic(){
+        music = Gdx.audio.newMusic(Gdx.files.internal("newsong.mp3"));
+        music.setLooping(true);
+        music.setVolume(0.1f);
+        music.play();
+
+    }
+    public void musicStop(){
+        music.stop();
+    }
+
+    public void createFixture(Body obstacleBody, PolygonShape bodyShape) {
+        obstacleBody.createFixture(bodyShape,4.0f);
+    }
+}
